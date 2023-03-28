b4b8ef3cba34951162910bde42a8d5741463d94f
Cchavesss <129199622+Cchavesss@users.noreply.github.com>
2023-03-28 11:37:00 -0400

    Add files via upload

---
+++ sketch_230328b.js
@@ -0,0 +1,44 @@
+function setup() {
+  createCanvas(600, 600);
+  background(100);
+}
+
+function draw() {
+    fill(50);
+  rect(120,400, 150,20,100)
+    fill(50);
+  rect(340,400, 150,20,100)
+  
+    fill(50);
+  rect(240,440, 20,150,100)
+    fill(50);
+  rect(340,440, 20,150,100)
+  
+    fill(150);
+  rect(200,350, 200,200,70)
+   
+  
+  fill(100);
+  rect(225,380, 150,150,70)
+   
+  fill(200);
+  arc(260, 255, 80, 200, PI, TWO_PI);
+     fill(200);
+  arc(340, 255, 80, 200, PI, TWO_PI);
+  
+  fill(200);
+  circle(300, 300, 200);
+    fill(250);
+  circle(250,280, 50)  
+  circle(340,280, 50) 
+  fill(50);
+  circle(250,280, 30)  
+  circle(340,280, 30) 
+  
+    fill(150);
+  rect(300,310, 70,20,40)
+  
+  fill(255);
+  arc(300, 360, 100, 50, PI, TWO_PI);
+  
+}
